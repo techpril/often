e7dd644195330ea202b542b9259c7eadc59f6a5b
techpril <priscila.teodorio2015@gmail.com>
2023-01-26 23:23:34 -0300

    Inicando back end com fastify e prisma, primeira rota com typescriopt

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient
+
+app.get('/', () => {
+    return 'Hey'
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server Running')
+})
