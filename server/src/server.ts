c21212173a328c3e77deebb290bb12b1a2445c80
techpril <priscila.teodorio2015@gmail.com>
2023-01-30 09:32:54 -0300

    Finalizando inicio do backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,25 @@
 import Fastify from 'fastify'
 import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
 
 const app = Fastify()
 const prisma = new PrismaClient
 
+app.register(cors)
+
 app.get('/', () => {
     return 'Hey'
+})
+
+app.get('/habits', async() => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Beber'
+            }
+        }
+    })
+    return habits
 })
 
 app.listen({
