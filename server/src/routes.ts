0fff27e8dc9ae02c463cf680ad34a1a8efccfbcd
unknown <priscila.teodorio2015@gmail.com>
2023-02-07 09:03:45 -0300

    Preparando as seeds

---
+++ server/src/routes.ts
@@ -0,0 +1,21 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from "./lib/prisma"
+
+export async function appRoutes(app: FastifyInstance){
+
+    app.get('/', () => {
+        return 'Hey'
+    })
+
+    app.get('/habits', async() => {
+        const habits = await prisma.habit.findMany({
+            // where: {
+            //     title: {
+            //         startsWith: 'Me '
+            //     }
+            // }
+        })
+        return habits
+    })
+
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,26 +1,11 @@
 import Fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient
 
 app.register(cors)
-
-app.get('/', () => {
-    return 'Hey'
-})
-
-app.get('/habits', async() => {
-    const habits = await prisma.habit.findMany({
-        where: {
-            title: {
-                startsWith: 'Me '
-            }
-        }
-    })
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333,
