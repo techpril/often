8a9993d89bb136d1c51e7fe950bdb0d3a09c7540
unknown <priscila.teodorio2015@gmail.com>
2023-02-14 09:05:40 -0300

    backend create new habit

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,18 +1,72 @@
 import { FastifyInstance } from 'fastify'
+import dayjs from 'dayjs'
+import { z } from 'zod'
 import { prisma } from "./lib/prisma"
 
 export async function appRoutes(app: FastifyInstance){
 
-    app.get('/', () => {
+    app.get('/', async() => {
         return 'Hey'
     })
 
     //Criar habito
-    app.post('/habits', async() => {
+    app.post('/habits', async(request) => {
+        const createHabitBody = z.object({
+            title: z.string(),
+            weekDays: z.array(
+                z.number().min(0).max(6)
+            )
+        })
+        const { title, weekDays } = createHabitBody.parse(request.body) 
 
+        const today = dayjs().startOf('day').toDate()
 
+        await prisma.habit.create({
+            data: {
+                title,
+                created_at: new Date(),
+                weekDays: {
+                    create: weekDays.map(weekDay => {
+                        return {
+                            week_day: weekDay,
+                        }
+                    })
+                    
+                }
+            }
+        })
+    })
 
-        
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(request.query)
+
+        const weekDay = dayjs(date).get('day')
+
+        //todos hábitos possíveis naquele dia
+        //hábitos que foram completados
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                },
+                weekDays:{
+                    some: {
+                        week_day: weekDay,
+                    }
+                }
+            },
+        })
+
+        return {
+            possibleHabits,
+        }
     })
 
 }
+
+
