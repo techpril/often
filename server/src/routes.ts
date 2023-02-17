469bcb77a591b5f2b6a351720be6e1b34f16364f
techpril <priscila.teodorio2015@gmail.com>
2023-02-17 18:21:03 -0300

    Hábitos completados back-end

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -44,11 +44,13 @@ export async function appRoutes(app: FastifyInstance){
 
         const { date } = getDayParams.parse(request.query)
 
-        const weekDay = dayjs(date).get('day')
+        const parsedDate = dayjs(date).startOf('day')
+
+        const weekDay = parsedDate.get('day')
+
 
         //todos hábitos possíveis naquele dia
-        //hábitos que foram completados
-
+        
         const possibleHabits = await prisma.habit.findMany({
             where: {
                 created_at: {
@@ -57,13 +59,31 @@ export async function appRoutes(app: FastifyInstance){
                 weekDays:{
                     some: {
                         week_day: weekDay,
-                    }
+                    },
                 }
             },
         })
 
+        //hábitos que foram completados
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate(),
+
+            },
+            include: {
+                dayHabits: true,
+            }
+
+        })
+
+        const completedHabits = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id
+        })
+
         return {
             possibleHabits,
+            completedHabits
         }
     })
 
