e4e1d29aa02883f45907614d2672d4491b0672c5
techpril <priscila.teodorio2015@gmail.com>
2023-02-23 17:27:34 -0300

    continuando front

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,29 @@
 import './styles/global.css';
 
-import Habits from "./components/Habits"
+// import Habits from "./components/Habits"
 
-function App() {
-  
+import logoImage from './assets/habits.svg'
+
+export function App() {
 
   return (
-    <div className="App">
-      <Habits completed={1}/>
-      <Habits completed={3}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
+          <img src={logoImage} alt="Habits" />
 
+          <button
+           type="button"
+           className="border border-violet-500 font-semibold rounded-lg px-6 py-4"
+          >
+            Novo hábito
+          </button>
+
+        </div>
+      </div>
        
     </div>
   )
+
 }
 
-export default App
