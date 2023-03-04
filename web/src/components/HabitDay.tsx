7d1dfd3a8d616ac716240b5fca10468a06b65986
techpril <priscila.teodorio2015@gmail.com>
2023-03-03 21:25:10 -0300

    cores diferentes nos habitos

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,5 +1,6 @@
 import * as Popover from '@radix-ui/react-popover';
 import { ProgressBar } from './ProgressBar';
+import clsx from 'clsx';
 
 interface HabitDayProps {
   completed: number,
@@ -12,7 +13,17 @@ export function HabitDay({completed, amount}: HabitDayProps) {
 
     return (
       <Popover.Root>
-        <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+        <Popover.Trigger 
+          className={clsx('w-10 h-10 border-2 rounded-lg', {
+            ' bg-zinc-900 border-zinc-800 ': completedPercentage === 0 ,
+            'bg-violet-900 border-violet-800': completedPercentage > 0 && completedPercentage < 20,
+            'bg-violet-800 border-violet-700': completedPercentage >= 20 && completedPercentage < 40,
+            'bg-violet-700 border-violet-600': completedPercentage >= 40 && completedPercentage < 60,
+            'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
+            'bg-violet-500 border-violet-400': completedPercentage >= 80,
+
+          })}
+        />
 
         <Popover.Portal>
           <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
