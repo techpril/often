f4ba7bd6323ec3944df8ae3ff3a8ff262ce4dbcf
techpril <priscila.teodorio2015@gmail.com>
2023-03-09 11:34:11 -0300

    toggle habits completed

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -79,7 +79,7 @@ export async function appRoutes(app: FastifyInstance){
 
         const completedHabits = day?.dayHabits.map(dayHabit => {
             return dayHabit.habit_id
-        })
+        }) ?? []
 
         return {
             possibleHabits,
