e8f5ab60588af9429ca8e514d470328af969598f
techpril <priscila.teodorio2015@gmail.com>
2023-03-11 11:44:24 -0300

    Finalizando front, focus, transitions, etc

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -4,78 +4,80 @@ import { Check } from 'phosphor-react';
 import { useEffect, useState } from 'react';
 import { api } from '../lib/axios';
 
-interface HabitsListProps {
+interface HabitListProps {
   date: Date
+  onCompletedChanged: (completed: number) => void
 }
 
 interface HabitsInfo {
-  possibleHabits:{
+  possibleHabits: {
     id: string;
     title: string;
-    created_at: string;
-  } [],
+    created_at: string
+  }[]
   completedHabits: string[]
 }
-
-export function HabitsList({date}: HabitsListProps){
+export function HabitsList({ date, onCompletedChanged }: HabitListProps) {
 
   const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>()
 
   useEffect(() => {
-    api.get('day',{
+    api.get('day', {
       params:{
         date: date.toISOString()
       }
     }).then(response => {
       setHabitsInfo(response.data)
     })
-  }, [])
+  },[])
 
   async function handleToggleHabit(habitId: string) {
-
     const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId)
-    
+  
     await api.patch(`/habits/${habitId}/toggle`)
 
     let completedHabits: string[] = []
 
-    if (isHabitAlreadyCompleted){
+    if(isHabitAlreadyCompleted) {
       completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId)
-    }else {
+    } else {
       completedHabits = [...habitsInfo!.completedHabits, habitId]
     }
-   
+
     setHabitsInfo({
       possibleHabits: habitsInfo!.possibleHabits,
-      completedHabits,
+      completedHabits
     })
+
+    onCompletedChanged(completedHabits.length)
   }
 
   const isDateInPast = dayjs(date).endOf('day').isBefore(new Date())
 
-  return(
-    <div className="mt-6 flex flex-col gap-3">
+  return (
+    <div className='mt-6 flex flex-col gap-3'>
       {habitsInfo?.possibleHabits.map(habit => {
         return (
-          <Checkbox.Root 
+          <Checkbox.Root
             key={habit.id}
             onCheckedChange={() => handleToggleHabit(habit.id)}
             checked={habitsInfo.completedHabits.includes(habit.id)}
             disabled={isDateInPast}
-            className="flex items-center gap-3 group"
+            className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed'
           >
-            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'>
               <Checkbox.Indicator>
-                <Check size={20} className="text-white "/>
+                <Check size={20} className="text-white" />
               </Checkbox.Indicator>
             </div>
 
-            <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-              Beber 2L de água
+            <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
+              {habit.title}
             </span>
           </Checkbox.Root>
-            )
-          })}
+        )
+      })}
+      
     </div>
   )
 }
